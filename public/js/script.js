2d5935d9808f26dcf189f7ddc9a95acc7cc3fb84
Alfredo-Padilla <12alfredopadilla12@gmail.com>
2021-10-03 18:33:09 +0200

    Mas cambios de maquetacion

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-     /* FALSO SCROLL */
-     function getScrollPercent() {
-        var h = document.documentElement,
-            b = document.body,
-            st = 'scrollTop',
-            sh = 'scrollHeight';
-        return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    /* ESCRITORIO */
+    if (window.innerWidth > 991) {
+        /* ESCONDER MENU */
+        window.addEventListener('wheel', (ev) => {
+            console.log(ev)
+            if( ev.deltaY > 0 ) {
+                document.querySelector('header').classList.add('off')
+            } else {
+                document.querySelector('header').classList.remove('off')
+            }
+        });
+        
     }
-    let f_scroll =  () => document.querySelector('#progreso-scroll').style.width = getScrollPercent() + '%';
-    f_scroll();
-    window.addEventListener('scroll', f_scroll);
+    /* FALSO SCROLL */
+    function getScrollPercent() {
+       var h = document.documentElement,
+           b = document.body,
+           st = 'scrollTop',
+           sh = 'scrollHeight';
+       return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+   }
+   let f_scroll =  () => document.querySelector('#progreso-scroll').style.width = getScrollPercent()*-1 +100 + '%';
+   f_scroll();
+   window.addEventListener('scroll', f_scroll);
 
     /* SUBMENUS */
     for (padre of document.querySelectorAll('.padre-desplegable')) {
